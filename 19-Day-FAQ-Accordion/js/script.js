7a85aebe5cf5786ffd7038612d02164ab7e728f8
GarySZ <garysoliz07@gmail.com>
2024-01-29 20:40:59 -0400

    day 19

---
+++ 19-Day-FAQ-Accordion/js/script.js
@@ -0,0 +1,15 @@
+const accordions = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < accordions.length; i++) {
+    accordions[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+
+        const panel = this.nextElementSibling;
+        if( panel.style.maxHeight ){
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
